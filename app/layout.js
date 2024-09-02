e0daadeb3953665d7167a326b81fabf7a937a8aa
TomiMalamud <tomasmalamud@gmail.com>
2024-09-02 19:24:34 -0300

    analytics

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
